a032203eafe11eec7e9ef8be6438b51a12b9e50e
Thomas <thomas@robboto.com>
2021-04-30 14:55:45 +0200

    Frontend landing page design with livewire blade component.

---
+++ resources/views/livewire/blog-card.blade.php
@@ -0,0 +1,70 @@
+<div class="p-10 sm:p-10 md:p-16">
+    <div class="grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-10">
+        <div class="rounded shadow-lg">
+            <a href="#">
+                <div class="relative">
+                    <img class="w-full" src="https://images.pexels.com/photos/196667/pexels-photo-196667.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                         alt="Sunset in the mountains">
+                </div>
+            </a>
+            <div class="px-6 py-4">
+                <a href="#" class="font-semibold text-lg inline-block hover:text-indigo-600 transition duration-500 ease-in-out">Best
+                    View in Newyork City</a>
+                <p class="text-gray-500 text-sm">
+                    The city that never sleeps
+                </p>
+            </div>
+            <div class="px-6 py-4">
+                <span href="#" class="flex justify-between items-center py-1 text-sm font-regular text-gray-900 mr-1">
+                  <span>Author:</span>
+                  <span class="ml-1">6 mins ago</span>
+                </span>
+            </div>
+        </div>
+
+        <div class="rounded overflow-hidden shadow-lg">
+            <a href="#">
+                <div class="relative">
+                    <img class="w-full" src="https://images.pexels.com/photos/1653877/pexels-photo-1653877.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                         alt="Sunset in the mountains">
+                </div>
+            </a>
+            <div class="px-6 py-4">
+                <a href="#" class="font-semibold text-lg inline-block hover:text-indigo-600 transition duration-500 ease-in-out">Best
+                    Pizza in Town</a>
+                <p class="text-gray-500 text-sm">
+                    The collection of best pizza images in Newyork city
+                </p>
+            </div>
+            <div class="px-6 py-4">
+                <span href="#" class="flex justify-between items-center py-1 text-sm font-regular text-gray-900 mr-1">
+                  <span>Author:</span>
+                  <span class="ml-1">3 mins read</span>
+                </span>
+            </div>
+        </div>
+
+        <div class="rounded overflow-hidden shadow-lg">
+            <a href="#">
+                <div class="relative">
+                    <img class="w-full" src="https://images.pexels.com/photos/257816/pexels-photo-257816.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                         alt="Sunset in the mountains">
+                </div>
+            </a>
+            <div class="px-6 py-4">
+                <a href="#"
+                   class="font-semibold text-lg inline-block hover:text-indigo-600 transition duration-500 ease-in-out">Best
+                    Salad Images ever</a>
+                <p class="text-gray-500 text-sm">
+                    The collection of best salads of town in pictures
+                </p>
+            </div>
+            <div class="px-6 py-4">
+                <span href="#" class="flex justify-between items-center py-1 text-sm font-regular text-gray-900 mr-1">
+                  <span>Author:</span>
+                  <span class="ml-1">6 mins read</span>
+                </span>
+            </div>
+        </div>
+    </div>
+</div>
